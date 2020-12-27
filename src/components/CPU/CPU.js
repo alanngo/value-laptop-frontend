d97b2728163eaa9a064dd5ad1aa3f25f4683e1cd
Alan <alan@remote.com>
2020-12-27 11:34:57 -0600

    nvidia and amd dropdown

--- src/components/CPU/CPU.js
+++ src/components/CPU/CPU.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { DropdownButton, ButtonGroup, Dropdown, Spinner} from 'react-bootstrap'
+import {  Button, ButtonGroup, Dropdown, Spinner} from 'react-bootstrap'
 import DisplayLaptop from '../DisplayLaptop/DisplayLaptop'
 import axios from 'axios';
 class CPU extends Component 
@@ -9,7 +9,7 @@ class CPU extends Component
 	{
 		price: '0',
 		laptops: [],
-		clicked: false
+    clicked: false,
   }
   
   onChange = (event) =>
@@ -18,12 +18,27 @@ class CPU extends Component
     this.setState({clicked: true})
     const URL = `https://value-laptop-backend.herokuapp.com/laptop/cpu/${event}` 
     axios.get(URL)
-	.then(res =>
-	{
-		// console.log(res.data)
-		this.setState({laptops : Object.values(res.data)})
-		console.log(this.state.laptops)
-	}).catch(err => console.log(err))
+    .then(res =>
+    {
+      // console.log(res.data)
+      this.setState({laptops : Object.values(res.data)})
+      console.log(this.state.laptops)
+    }).catch(err => console.log(err))
+  }
+
+  onClick = (e) =>
+  {
+    e.preventDefault()
+    console.log(e)
+    this.setState({clicked: true})
+    const URL = `https://value-laptop-backend.herokuapp.com/laptop/cpu/${e.target.value}` 
+    axios.get(URL)
+    .then(res =>
+    {
+      // console.log(res.data)
+      this.setState({laptops : Object.values(res.data)})
+      console.log(this.state.laptops)
+    }).catch(err => console.log(err))
   }
 
   render()
@@ -31,17 +46,27 @@ class CPU extends Component
     return (
       <div className="CPU">
         <h3>Choose a CPU </h3>
-        <DropdownButton as={ButtonGroup} size="lg" title="Intel" id="bg-vertical-dropdown-1" variant="primary" onSelect={this.onChange}>
+        <Dropdown as={ButtonGroup} size="lg" id="bg-vertical-dropdown-1" variant="primary" onSelect={this.onChange}>
+          <Button onClick={this.onClick} value="i">Intel</Button>
+          <Dropdown.Toggle split variant="primary" id="dropdown-split-basic" />
+          <Dropdown.Menu>
           <Dropdown.Item eventKey="i5">core i5 (standard)</Dropdown.Item>
           <Dropdown.Item eventKey="i7">core i7 (high performance)</Dropdown.Item>
           <Dropdown.Item eventKey="i9">core i9 (enthusiast)</Dropdown.Item>
           <Dropdown.Item eventKey="Xeon">xeon (workstation)</Dropdown.Item>
-        </DropdownButton>
-        <DropdownButton as={ButtonGroup} size="lg" title="AMD" id="bg-vertical-dropdown-2" variant="danger" onSelect={this.onChange}>
-          <Dropdown.Item eventKey="ryzen 5">Ryzen 5 (standard)</Dropdown.Item>
-          <Dropdown.Item eventKey="ryzen 7">Ryzen 7 (high performance)</Dropdown.Item>
-          <Dropdown.Item eventKey="ryzen 9">Ryzen 9 (enthusiast)</Dropdown.Item>
-        </DropdownButton>
+        </Dropdown.Menu>
+        </Dropdown>
+
+
+        <Dropdown as={ButtonGroup} size="lg" id="bg-vertical-dropdown-1" variant="danger" onSelect={this.onChange}>
+          <Button onClick={this.onClick} variant="danger" value="ryzen">AMD</Button>
+          <Dropdown.Toggle split variant="danger" id="dropdown-split-basic" />
+          <Dropdown.Menu>
+          <Dropdown.Item eventKey="Ryzen 5">Ryzen 5 (standard)</Dropdown.Item>
+          <Dropdown.Item eventKey="Ryzen 7">Ryzen 7 (high performance)</Dropdown.Item>
+          <Dropdown.Item eventKey="Ryzen 9">Ryzen 9 (enthusiast)</Dropdown.Item>
+        </Dropdown.Menu>
+        </Dropdown>
         <br/>
         {
 				(this.state.clicked === false)?<></>:
--- src/components/GPU/GPU.js
+++ src/components/GPU/GPU.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import DisplayLaptop from '../DisplayLaptop'
-import  {DropdownButton, Dropdown, Spinner, ButtonGroup}  from 'react-bootstrap';
+import  { Dropdown, Button, Spinner, ButtonGroup}  from 'react-bootstrap';
 import axios from 'axios'
 
 class GPU extends Component 
@@ -27,46 +27,71 @@ class GPU extends Component
     }).catch(err => console.log(err))
   }
 
-  handleClick = (e) =>
-  
+  handleClick = (e) =>  
   {
     e.preventDefault()
+    console.log(e.target.value)
+    let list = e.target.value.split(',')
+    console.log(list)
+    let tmp = []
     this.setState({clicked: true})
-    const URL = `https://value-laptop-backend.herokuapp.com/laptop/gpu/quadro` 
-    axios.get(URL)
-    .then(res =>
+    list.forEach(elem => 
     {
-      // console.log(res.data)
-      this.setState({laptops : Object.values(res.data)})
-      console.log(this.state.laptops)
-    }).catch(err => console.log(err))
+      console.log(`elem: ${elem}`)
+      const URL = `https://value-laptop-backend.herokuapp.com/laptop/gpu/${elem}` 
+      console.log(URL)
+      axios.get(URL)
+      .then(res =>
+      {
+        let data = Object.values(res.data)
+        data.forEach(d => tmp.push(d))
+        tmp.sort((a, b) => b.price - a.price)
+        this.setState({laptops : tmp})
+      })
+      .then(() => console.log(tmp))
+      .catch(err => console.log(err))
+    })
+
+  }
+
+  NVIDIA = ["MX", "1050", "1650", "1060",  "1660", "2060", "1070",  "2070", "1080",  "2080", "Quadro"]
+  AMD = ["vega", "560"]
+
+  getCategory = (elem) => 
+  {
+    if (elem.includes("MX")||elem.includes("vega")) return "budget"
+    if (elem.includes("50")) return "entry level"
+    if (elem.includes("60")) return "standard"
+    if (elem.includes("70")) return "hi-performance"
+    if (elem.includes("80")) return "enthusiast"
+    if (elem.includes("Quadro")) return "workstation"
   }
   render()
   {
     return (
       <div className="GPU">
         <h3>Choose a GPU </h3>
-        <DropdownButton as={ButtonGroup} size="lg" title="Nvidia" id="bg-vertical-dropdown-1" variant="success" onSelect={this.onChange}>
-          <Dropdown.Item eventKey="mx">MX series (budget)</Dropdown.Item>
-          <Dropdown.Divider />
-          <Dropdown.Item eventKey="1050">GTX 1050/1050 Ti (entry level)</Dropdown.Item>
-          <Dropdown.Item eventKey="1650">GTX 1650/1650 Super (entry level)</Dropdown.Item>
-          <Dropdown.Item eventKey="1060">GTX 1060 (standard)</Dropdown.Item>
-          <Dropdown.Item eventKey="1660">GTX 1660/1660 Super/1660 Ti (standard)</Dropdown.Item>
-          <Dropdown.Item eventKey="1070">GTX 1070/1070 Ti (high performance)</Dropdown.Item>
-          <Dropdown.Item eventKey="1080">GTX 1080/1080 Ti (enthusiast)</Dropdown.Item>
-          <Dropdown.Divider />
-          <Dropdown.Item eventKey="2060">RTX 2060/2060 Super (standard)</Dropdown.Item>
-          <Dropdown.Item eventKey="2070">RTX 2070/2070 Super (high performance)</Dropdown.Item>
-          <Dropdown.Item eventKey="2080">RTX 2080/2080 Super/2080 Ti (enthusiast)</Dropdown.Item>
-          <Dropdown.Divider />
-          <Dropdown.Item eventKey="Quadro">Quadro (workstation)</Dropdown.Item>
-        </DropdownButton>
-        <DropdownButton as={ButtonGroup} size="lg" title="AMD" id="bg-vertical-dropdown-1" variant="danger" onSelect={this.onChange}>
-          <Dropdown.Item eventKey="vega">Vega (budget)</Dropdown.Item>
-          <Dropdown.Divider />
-          <Dropdown.Item eventKey="560">RX 560/5600 series (standard)</Dropdown.Item>
-        </DropdownButton>
+        <Dropdown as={ButtonGroup} size="lg" id="bg-vertical-dropdown-1" variant="success" onSelect={this.onChange}>
+          <Button 
+          variant="success" onClick={this.handleClick} value={this.NVIDIA}>Nvidia</Button>
+          <Dropdown.Toggle split variant="success" id="dropdown-split-basic" />
+          <Dropdown.Menu>
+            {
+              this.NVIDIA.map(elem => <Dropdown.Item eventKey={elem}>{elem} ({this.getCategory(elem)})</Dropdown.Item>)
+            }
+          </Dropdown.Menu>
+        </Dropdown>
+        
+        <Dropdown as={ButtonGroup} size="lg" id="bg-vertical-dropdown-1" variant="danger" onSelect={this.onChange}>
+          <Button onClick={this.handleClick} variant="danger" value={this.AMD}>AMD</Button>
+          <Dropdown.Toggle split variant="danger" id="dropdown-split-basic" />
+            <Dropdown.Menu>
+            {
+              this.AMD.map(elem => <Dropdown.Item eventKey={elem}>{elem} ({this.getCategory(elem)})</Dropdown.Item>)
+            }
+        </Dropdown.Menu>
+        </Dropdown>
+
         <br/>
         {
 				(this.state.clicked === false)?<></>:
